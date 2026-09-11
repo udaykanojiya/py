ff6c38001521c46e891faeac43e6302a764f3804
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-09-11 17:34:01 +0530

    chap2

--- chapter2.py
+++ chapter2.py
@@ -1,3 +1,4 @@
+import math
 a = 3
 b = 4.5
 c = "Hello, World!"
@@ -8,5 +9,16 @@ print(type(a))
 
 print(str(a) + " is of type " + str(type(a)))
 
-f = int(input("Enter a number: "))
-print(f"{f} is of type {type(f)}")
+# f = int(input("Enter a number: "))
+# print(f"{f} is of type {type(f)}")
+
+a = 34
+b = 80
+if(a>b):
+    print(f"{a} is greater than {b}")
+else:
+    print(f"{a} is less than {b}")
+
+print((a+b)/2)
+
+print(math.pow(2, 4))
