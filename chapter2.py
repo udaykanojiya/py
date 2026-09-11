8464b94cff02d1b9bb4d815ce416facde875c4c7
Uday Kanojiya <udaykanojiya91@gmail.com>
2026-09-11 16:58:18 +0530

    chap2

---
+++ chapter2.py
@@ -0,0 +1,12 @@
+a = 3
+b = 4.5
+c = "Hello, World!"
+d = True
+e = None
+
+print(type(a))
+
+print(str(a) + " is of type " + str(type(a)))
+
+f = int(input("Enter a number: "))
+print(f"{f} is of type {type(f)}")
